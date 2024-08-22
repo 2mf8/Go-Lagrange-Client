c4c4701757566d5965c70c620a8174f8b48519d7
一仝 <chun5060@outlook.com>
2024-08-22 19:06:53 +0800

    update

--- pkg/config/setting.go
+++ pkg/config/setting.go
@@ -31,10 +31,9 @@ func ReadSetting() Setting {
 	tomlData := `# linux / macos / windows, 默认linux
 Platform = "linux"
 # linux[3.1.2-13107,3.2.10-25765] macos[6.9.20-17153] windows[9.9.12-25493]
-AppVersion = "3.1.2-13107"
-# 默认 linux 3.1.2-13107 可用 master:https://sign.lagrangecore.org/api/sign,Mirror:https://sign.0w0.ing/api/sign
-# linux 3.2.10-25765 暂不提供SignServer
-SignServer = "https://sign.lagrangecore.org/api/sign"
+AppVersion = "3.2.10-25765"
+# 默认 linux 3.2.10-25765 可用 master:https://sign.lagrangecore.org/api/sign,Mirror:https://sign.0w0.ing/api/sign/25765
+SignServer = "https://sign.lagrangecore.org/api/sign/25765"
 	`
 	if !util.PathExists(SettingPath) {
 		if err := os.MkdirAll(SettingPath, 0777); err != nil {
@@ -45,12 +44,11 @@ SignServer = "https://sign.lagrangecore.org/api/sign"
 	_, err := os.Stat(fmt.Sprintf("%s/setting.toml", SettingPath))
 	if err != nil {
 		_ = os.WriteFile(fmt.Sprintf("%s/setting.toml", SettingPath), []byte(tomlData), 0644)
-		log.Warn("已生成配置文件 conf.toml ,请修改后重新启动程序。")
-		log.Info("该程序将于5秒后退出！")
+		log.Warn("已生成配置文件 conf.toml ,将于5秒后进入登录流程。")
+		log.Info("也可现在退出程序，修改 conf.toml 后重新启动。")
 		time.Sleep(time.Second * 5)
-		os.Exit(1)
+		//os.Exit(1)
 	}
 	AllSetting = AllSettings()
-	fmt.Println(AllSetting.AppVersion, AllSetting.Platform, AllSetting.SignServer)
 	return *AllSetting
 }
